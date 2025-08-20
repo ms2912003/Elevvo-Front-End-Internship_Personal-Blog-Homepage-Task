6dd7632290a4fa9cb3666fb4ea4dbe6ce427d36e
Menna Saad <ma90230@gmail.com>
2025-08-21 02:46:39 +0300

    Initial commit - my blog project

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+function Header () {
+    return(
+    <header className = "text-center p-20 bg-purple-700 text-white">
+        <h1 className = "text-3xl font-bold"> My Personal Blog </h1>
+    </header>
+    );
+}
+
+export default Header;
